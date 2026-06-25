3f7fd7a18b88efa24f57037f7bd17391ea4294f2
Michael <michael@ITAD_platform.com>
2026-06-25 10:54:23 -0400

    im going to kill myself

--- pcisearch/main.go
+++ pcisearch/main.go
@@ -71,6 +71,6 @@ func lookupClass(class string, sources []string) {
 			os.Exit(0)
 		}
 	}
-	fmt.Printf("Failed to find %s class\n", sources)
+	fmt.Printf("Failed to find %s class\n", class)
 	os.Exit(1)
 }
